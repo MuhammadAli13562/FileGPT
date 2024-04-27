8d090bba861de82bb9bedcf90385631b88c4588a
MuhammadAli13562 <m.ali8.aj8@gmail.com>
2024-04-27 11:40:48 +0500

    client init -- routes done

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignInForm from "./_auth/pages/SignInForm";
+import SignUpForm from "./_auth/pages/SignUpForm";
+import LandingPage from "./_root/pages/LandingPage";
+import ContextWindow from "./_root/pages/ContextWindow";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routes */}
+          <Route element={<AuthLayout />}>
+            <Route index element={<LandingPage />} />
+            <Route path="/sign-in" element={<SignInForm />} />
+            <Route path="/sign-up" element={<SignUpForm />} />
+          </Route>
+          {/* Private Routes */}
+          <Route element={<RootLayout />}>
+            <Route path="/chat/:id" element={<ContextWindow />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/client/src/_root/pages/ContextWindow.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContextWindow = () => {
+  return <div>ContextWindow</div>;
+};
+
+export default ContextWindow;

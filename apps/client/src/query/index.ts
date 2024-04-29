aae486beb7e05e2f0188072941d4e7db5b432d5a
MuhammadAli13562 <m.ali8.aj8@gmail.com>
2024-04-29 23:24:54 +0500

    hcoded stream query

--- apps/client/src/query/index.ts
+++ apps/client/src/query/index.ts
@@ -5,7 +5,7 @@ import { AddMessage } from "src/redux/recipies/StreamUpdate";
 
 export const StreamQuery = async (input: queryInputType, dispatch: any) => {
   for await (let chunk of streamingFetch(() =>
-    fetch(ServerEndpoint + "user/query", {
+    fetch("https://chatapp.ali-98-ec2-backend.click/query", {
       method: "POST",
       headers: {
         token: localStorage.getItem("token") as string,
